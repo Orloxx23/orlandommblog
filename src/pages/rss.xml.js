0a1c4980cd910b88378147640c4972f9dc7a23b9
Orloxx23 <madorlix@gmail.com>
2023-07-15 19:02:37 -0500

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -11,7 +11,7 @@ export async function get(context) {
     stylesheet: "/rss/styles.xsl",
     items: posts.map((post) => ({
       ...post.data,
-      link: `/posts/${post.slug}/`,
+      link: `/blog/${post.slug}/`,
     })),
   });
 }
